b805a5317e37bc06360ac17c76b687c11885ad17
Precious Idubor <preciousidubor@gmail.com>
2022-10-11 15:29:50 -0700

    Square area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+    Defines a square
+"""
+
+
+class Square:
+    """
+        Representation of a square
+    """
+    def __init__(self, size=0):
+        """Initialization
+            Args:
+                size (int): of square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Returns the current square area
+        """
+        return (self.__size ** 2)
