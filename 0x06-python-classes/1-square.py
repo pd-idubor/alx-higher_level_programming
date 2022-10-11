de8746a15af4857a136d5302c3aa002b837c89e4
Precious Idubor <preciousidubor@gmail.com>
2022-10-11 14:48:16 -0700

    Instantiation

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+    Defines a square
+"""
+
+
+class Square:
+    """
+        Attributes:
+            size (int): Determines square size
+    """
+    def __init__(self, size):
+        """
+            Args:
+                size (int): of square
+        """
+        self.__size = size
