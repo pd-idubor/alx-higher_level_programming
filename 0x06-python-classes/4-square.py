4117acc6b9cc7d8668d19ff84671c7d53f6204b5
Precious Idubor <preciousidubor@gmail.com>
2022-10-11 15:44:36 -0700

    Getter and setter properties

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+    Defines a square
+"""
+
+
+class Square:
+    """
+        Representation of a square
+    """
+    def __init__(self, size=0):
+        """Initialization
+            Args:
+                size (int): of square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Retrieves square size
+        """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Sets size of square
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if (value < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Returns the current square area
+        """
+        return (self.__size ** 2)
