4fea657409044e082b84480c63356e9820892190
Precious Idubor <preciousidubor@gmail.com>
2022-10-17 14:02:44 -0700

    repr() functionality added

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""
+    Defines the representation of a rectangle
+"""
+
+
+class Rectangle:
+    """Defines a rectangle"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if (value < 0):
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if (value < 0):
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        if (self.__width == 0 or self.__height == 0):
+            return (0)
+        return (2 * (self.__height + self.__width))
+
+    def __str__(self):
+        rect = ""
+        if (self.__width == 0 or self.__height == 0):
+            return (rect)
+
+        for i in range(self.__height - 1):
+            rect += ("#" * self.__width + "\n")
+        rect += ("#" * self.__width)
+        return (rect)
+
+    def __repr__(self):
+        ret = "{}({}, {})".format(self.__class__.__name__,
+                                  self.width, self.height)
+        return (ret)
