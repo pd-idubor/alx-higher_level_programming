617b975ef71376f14c5c0ba0bb00854be1e04bfd
Precious Idubor <preciousidubor@gmail.com>
2022-10-24 19:34:18 -0700

    Subclass check

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines an identity check function"""
+
+
+def inherits_from(obj, a_class):
+    """Checks for instances of a class"""
+    if (issubclass(type(obj), a_class)):
+        return (True)
+    return (False)
