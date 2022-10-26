74f26e34b4767b0c889d2cf8c5a68f6ce91cfe85
Precious Idubor <preciousidubor@gmail.com>
2022-10-26 13:06:17 -0700

    Convert to json

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+    JSON representation returned
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """Returns the JSON representation of an object"""
+    return (json.dumps(my_obj))
