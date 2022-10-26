fc74b2f56f5a575441eb576b23488c88b10a0839
Precious Idubor <preciousidubor@gmail.com>
2022-10-26 12:37:02 -0700

    File reading

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,6 +6,5 @@
 
 def read_file(filename=""):
     """Reads a text file"""
-    with open('my_file_0.txt', encoding="utf-8") as r:
-        read_d = r.read()
-        print(read_d, end="")
+    with open(filename, encoding="utf-8") as r:
+        print(r.read(), end="")
