3c19c628f945a8b8c8a6d38d26c77240c0104bde
Precious Idubor <preciousidubor@gmail.com>
2022-10-27 00:47:22 -0700

    Student class + attrs retrieval

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    Defines a student class
+"""
+
+
+class Student:
+    """Student definition"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves dictionary rep of 'Student' class"""
+        if (type(attrs) == list and
+                all(type(item) == str for item in attrs)):
+            return {i: getattr(self, i) for i in attrs if hasattr(self, i)}
+        return (self.__dict__)
