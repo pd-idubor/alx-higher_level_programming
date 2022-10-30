78d13d3d377dcb4604c917fe65a130597b7069fc
Precious Idubor <preciousidubor@gmail.com>
2022-10-30 06:38:59 -0700

    Base class created

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    Defines 'Base' class
+"""
+
+
+class Base:
+    """Base class"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if (id is not None):
+            self.id = id
+        else:
+            type(self).__nb_objects += 1
+            self.id = type(self).__nb_objects
