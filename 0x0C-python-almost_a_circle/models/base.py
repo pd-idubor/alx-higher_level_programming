3bc2b412ab9744a4b96988ac11f4cd4b2a1fda5b
Precious Idubor <preciousidubor@gmail.com>
2022-10-31 15:59:16 -0700

    Json string representation of list dict

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """
     Defines 'Base' class
 """
+import json
 
 
 class Base:
@@ -9,8 +10,17 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """Class initialization"""
         if (id is not None):
             self.id = id
         else:
             type(self).__nb_objects += 1
             self.id = type(self).__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns the JSON string representation of list_dict"""
+        if (list_dictionaries is None or list_dictionaries == []):
+            return ("[]")
+
+        return (json.dumps(list_dictionaries))
