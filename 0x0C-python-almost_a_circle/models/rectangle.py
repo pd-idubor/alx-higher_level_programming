1c7333351a8171d5fd06f0c5cfad5f5b893ecd58
Precious Idubor <preciousidubor@gmail.com>
2022-10-31 13:25:19 -0700

    Validator included

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -8,18 +8,19 @@ from models.base import Base
 class Rectangle(Base):
     """Rectangle class"""
     def __init__(self, width, height, x=0, y=0, id=None):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
 
     @property
-    def width(self, value):
+    def width(self):
         return (self.__width)
 
     @width.setter
     def width(self, value):
+        self.validator("width", value)
         self.__width = value
 
     @property
@@ -28,6 +29,7 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
+        self.validator("height", value)
         self.__height = value
 
     @property
@@ -36,6 +38,7 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
+        self.validator("x", value)
         self.__x = value
 
     @property
@@ -44,4 +47,15 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
+        self.validator("y", value)
         self.__y = value
+
+    @staticmethod
+    def validator(attribute, value):
+        if (type(value) != int):
+            raise TypeError("{} must be an integer".format(attribute))
+        if (attribute == "x" or attribute == "y"):
+            if (value < 0):
+                raise ValueError("{} must be >= 0".format(attribute))
+        elif (value <= 0):
+            raise ValueError("{} must be > 0".format(attribute))
