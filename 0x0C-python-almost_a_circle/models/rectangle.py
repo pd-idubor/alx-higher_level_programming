10d83d8c975adb227544d518e59cf16a80e650b4
Precious Idubor <preciousidubor@gmail.com>
2022-10-31 14:10:05 -0700

    Printable rep

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -59,6 +59,10 @@ class Rectangle(Base):
         for x in range(self.height):
             print("#" * self.width)
 
+    def __str__(self):
+        return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
+                self.x, self.y, self.width, self.height))
+
     @staticmethod
     def validator(attribute, value):
         if (type(value) != int):
