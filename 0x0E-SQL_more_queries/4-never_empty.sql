f56b9d3ffce05f368a63b07a961b25e441e59104
Precious Idubor <preciousidubor@gmail.com>
2022-12-01 20:11:26 -0800

    Table

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,2 +1,2 @@
 -- Creates the table id_not_null
-CREATE TABLE IF NOT EXISTS id_not_null(id id INT DEFAULT 1, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS id_not_null(id id INT DEFAULT 1, name VARCHAR(256) NOT NULL);
