b761f97ed81c2ee7174e504787219cba974c1105
Precious Idubor <preciousidubor@gmail.com>
2023-01-04 13:05:09 -0800

    Binary filt

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,8 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE\
+                BINARY 'N%' ORDER BY id ASC")
 
     rows = cur.fetchall()
     for row in rows:
