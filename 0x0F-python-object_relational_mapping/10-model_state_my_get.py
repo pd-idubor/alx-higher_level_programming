1836f634e1ee74453027263f4e34ebdd1537833f
Precious Idubor <preciousidubor@gmail.com>
2023-01-05 14:25:09 -0800

    Get state, injection proof

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+    Lists all States objects from hbtn_0e_6_usa
+"""
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+import sys
+from sqlalchemy import create_engine
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2],
+                                  sys.argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    check = False
+    for state in session.query(State).order_by(State.id):
+        if state.name == sys.argv[4]:
+            print("{}".format(state.id))
+            check = True
+            break
+    if check is False:
+        print("Not found")
