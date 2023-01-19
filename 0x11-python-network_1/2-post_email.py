73d6cb0ca3082d66633d6c2d3f01e0d2a09d3d92
Precious Idubor <preciousidubor@gmail.com>
2023-01-19 05:00:52 -0800

    test

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 """Displays the value of a header response variable"""
 import urllib.request
+from urllib.parse import urlencode
 import sys
 
 
 if __name__ == "__main__":
     url = sys.argv[1]
     mail = {'email': sys.argv[2]}
-    req = urllib.request.Request(url, mail)
+    data = urlencode(mail)
+    data = data.encode('ascii')
+    
+    req = urllib.request.Request(url,data)
     with urllib.request.urlopen(req) as response:
-        page = response.read()
-        print("Your email is: {}".format(page))
+        resp = response.read()
+        print("Your email is: {}".format(resp.decode("utf-8")))
