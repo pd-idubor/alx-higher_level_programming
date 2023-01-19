ca67268ad7a085ede3a8cd02e5afb6fd797ca7fc
Precious Idubor <preciousidubor@gmail.com>
2023-01-19 03:48:32 -0800

    Fetch url

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""S1cript that returns a url"""
+import urllib.request
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+        html = response.read()
+        print(html)
+        print ("Body response:")
+        print ("\t-", "type:", type(html))
+        print ("\t-", "content:", html)
+        print ("\t-", "utf8 content:", html.decode('utf-8'))
