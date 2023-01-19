99313eac000cfe64ce0303230d893cc84130cbe2
Precious Idubor <preciousidubor@gmail.com>
2023-01-19 03:58:51 -0800

    handled

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,10 +4,11 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+    url = "https://alx-intranet.hbtn.io/status"
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
         html = response.read()
-        print(html)
-        print ("Body response:")
-        print ("\t-", "type:", type(html))
-        print ("\t-", "content:", html)
-        print ("\t-", "utf8 content:", html.decode('utf-8'))
+        print("Body response:")
+        print("\t- type: ".format(type(html)))
+        print("\t- content: ".format(html))
+        print("\t- utf8 content: ".format(html.decode('utf-8')))
