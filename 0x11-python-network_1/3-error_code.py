09d0afed727bf9061e6a3ec1d2fb8430753e8305
Precious Idubor <preciousidubor@gmail.com>
2023-01-19 05:17:55 -0800

    Error code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Displays response and handles error"""
+import urllib.request
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(req) as response:
+            data = response.read)
+            print(data.decode("utf-8"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
