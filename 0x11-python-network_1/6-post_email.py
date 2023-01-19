8776c74954f9e3843d6dedbfc37fa19caf733cf0
Precious Idubor <preciousidubor@gmail.com>
2023-01-19 06:15:20 -0800

    corr

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,6 +7,6 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     mail = {'email': sys.argv[2]}
-    
-    req = requests.post(url, mail)
+
+    req = requests.post(url, data=mail)
     print(req.text)
