592a1953af84991e146fafd67234ae316df866d3
Precious Idubor <preciousidubor@gmail.com>
2023-02-02 14:15:39 -0800

    More inheritance

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const Sq = require('./5-square');
+
+class Square extends Sq {
+  charPrint (c) {
+    if (typeof (c) === 'undefined') {
+      c = 'X';
+    } else {
+      c = 'C';
+    }
+
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
