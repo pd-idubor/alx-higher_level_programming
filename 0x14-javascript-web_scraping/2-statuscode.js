9f02943baf03852b0f7bac767b065b9e6dd1764d
Precious Idubor <preciousidubor@gmail.com>
2023-02-09 07:34:21 -0800

    Error handled

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,5 +4,6 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, function (error, response, body) {
+  if (error) return (error.statusCode);
   console.log('code:', response.statusCode);
 });
